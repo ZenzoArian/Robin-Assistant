4188a559c382fb9820b50c1269b904d76c46c3a3
JanHenst <42889823+JanHenst@users.noreply.github.com>
2021-03-08 16:03:49 +0100

    Choose client page done

---
+++ app/js/chooseClient.js
@@ -0,0 +1,51 @@
+syncButton = document.getElementById('syncButton');
+clientName1 = document.getElementById('clientName1');
+clientName2 = document.getElementById('clientName2');
+clientName3 = document.getElementById('clientName3');
+clientName4 = document.getElementById('clientName4');
+clientName5 = document.getElementById('clientName5');
+checkbox1 = document.getElementById('checkbox1');
+checkbox2 = document.getElementById('checkbox2');
+checkbox3 = document.getElementById('checkbox3');
+checkbox4 = document.getElementById('checkbox4');
+checkbox5 = document.getElementById('checkbox5');
+
+function handleInputCheckbox() {
+    if (checkbox1.checked || checkbox2.checked || checkbox3.checked || checkbox4.checked || checkbox5.checked) {
+        syncButton.style.color = '#fff';
+        syncButton.style.backgroundColor = '#928FDF';
+    } else {
+        syncButton.style.color = '#928FDF';
+        syncButton.style.backgroundColor = '#fff';
+    }
+
+    if (checkbox1.checked) {
+        clientName1.style.color = "#928FDF";
+    } else {
+        clientName1.style.color = "#1A1818";
+    }
+    
+    if (checkbox2.checked) {
+        clientName2.style.color = "#928FDF";
+    } else {
+        clientName2.style.color = "#1A1818";
+    }
+    
+    if (checkbox3.checked) {
+        clientName3.style.color = "#928FDF";
+    } else {
+        clientName3.style.color = "#1A1818";
+    }
+    
+    if (checkbox4.checked) {
+        clientName4.style.color = "#928FDF";
+    } else {
+        clientName4.style.color = "#1A1818";
+    }
+    
+    if (checkbox5.checked) {
+        clientName5.style.color = "#928FDF";
+    } else {
+        clientName5.style.color = "#1A1818";
+    }
+}
--- app/js/forgotpassword.js
+++ app/js/forgotpassword.js
@@ -1,33 +1,34 @@
 var x = document.getElementById("emailRecoverPW");
 let inputMail = document.getElementById('emailRecoverPW');
-
+let errorText = document.getElementById('error-text');
+let correctText = document.getElementById('correct-text');
+let passwordButton = document.getElementById('passwordButton');
 
 function recoverPasswordFunction() {
     
     if (x.value.length == 0) {
         document.getElementById('error-input-emailrecoverPW').style.display = 'unset';
-        document.getElementById('error-text').style.visibility = 'unset';
-        document.getElementById('error-text').style.display = 'block';
-        document.getElementById('emailRecoverPW').style.borderColor = 'rgb(229, 37, 98, .5)';
         document.getElementById('input--div').style.marginTop = '-18px';
         document.getElementById('help-text').style.marginTop = '-15px';
-        document.getElementById('error-text').style.marginTop = '-20px';
-        document.getElementById('error-text').style.marginBottom = '40px';
-
+        errorText.style.visibility = 'unset';
+        errorText.style.display = 'block';
+        inputMail.style.borderColor = 'rgb(229, 37, 98, .5)';
+        errorText.style.marginTop = '-20px';
+        errorText.style.marginBottom = '40px';
         inputMail.classList.add('your-class');
         return false;
     } else {
         document.getElementById('error-input-emailrecoverPW').style.display = 'none';
         document.getElementById('correct-input-emailrecoverPW').style.display = 'unset';
-        document.getElementById('error-text').style.visibility = 'unset';
-        document.getElementById('error-text').style.display = 'none';
-        document.getElementById('emailRecoverPW').style.borderColor = 'rgb(64, 130, 99, .5)';
-        document.getElementById('correct-text').style.visibility = 'unset';
-        document.getElementById('correct-text').style.display = 'block';
         document.getElementById('input--div').style.marginTop = '-18px';
         document.getElementById('help-text').style.marginTop = '-15px';
-        document.getElementById('correct-text').style.marginTop = '-20px';
-        document.getElementById('correct-text').style.marginBottom = '40px';
+        errorText.style.visibility = 'unset';
+        errorText.style.display = 'none';
+        inputMail.style.borderColor = 'rgb(64, 130, 99, .5)';
+        correctText.style.visibility = 'unset';
+        correctText.style.display = 'block';
+        correctText.style.marginTop = '-20px';
+        correctText.style.marginBottom = '40px';
         return true;
     }
 
@@ -35,21 +36,21 @@ function recoverPasswordFunction() {
 
 
 inputMail.oninput = handleInput;
-inputPassword.oninput = handleInput;
 
 function handleInput() {
     if (x.value.length > 0) {
-        document.getElementById('passwordButton').style.color = '#fff';
-        document.getElementById('passwordButton').style.backgroundColor = '#928FDF';
-        document.getElementById('error-text').style.display = 'block';
-        document.getElementById('error-text').style.visibility = 'hidden';
         document.getElementById('help-text').style.marginTop = '-15px';
-        document.getElementById('error-text').style.marginTop = '-20px';
-        document.getElementById('error-text').style.marginBottom = '40px';
+        passwordButton.style.color = '#fff';
+        passwordButton.style.backgroundColor = '#928FDF';
+        errorText.style.display = 'block';
+        errorText.style.visibility = 'hidden';
+        errorText.style.marginTop = '-20px';
+        errorText.style.marginBottom = '40px';
     } else {
-        document.getElementById('passwordButton').style.color = '#928FDF';
-        document.getElementById('passwordButton').style.backgroundColor = '#fff';
-        document.getElementById('error-text').style.display = 'none';
-        document.getElementById('error-text').style.visibility = 'unset';
+        document.getElementById('help-text').style.marginTop = '40px';
+        passwordButton.style.color = '#928FDF';
+        passwordButton.style.backgroundColor = '#fff';
+        errorText.style.display = 'none';
+        errorText.style.visibility = 'unset';
     }
 }
